c2098a5fd289863ad2c3bc194661768969896c7f
Javier <javmanfer@gmail.com>
2023-10-11 16:35:57 +0200

    Día 10-10

--- UD4/4b Arrays/code.js
+++ UD4/4b Arrays/code.js
@@ -24,3 +24,28 @@ for (let e of a) {
 }
 s += "</ul>";
 document.querySelector("#forof").innerHTML = s;
+
+
+//////////////////
+// Bucle map
+//////////////////
+
+s = "<ul>";
+a.map(x => {
+	s += `<li>${x}</li>`;
+	return x * x;
+});
+s += "</ul>";
+document.querySelector("#map").innerHTML = s;
+
+
+//////////////////
+// Bucle forEach
+//////////////////
+
+s = "<ul>";
+a.forEach( (x, i,a) => {
+	s += `<li>Elemento ${i} es ${x} y el siguiente es ${a[i+1]}</li>`;
+});
+s += "</ul>";
+document.querySelector("#foreach").innerHTML = s;
