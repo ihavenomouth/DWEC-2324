7492d0ffa1d50657abcc6068d7a35b6a94923c38
Javier <javmanfer@gmail.com>
2023-11-09 16:06:36 +0100

    Subido ejercicio examen

--- UD5/Eventos/Ejercicio 10/code.js
+++ UD5/Eventos/Ejercicio 10/code.js
@@ -1,9 +1,33 @@
 "use strict";
 
+const generarArrayDatos = contenido =>{
+  let a = contenido.split("\n");
+  a.shift(); // Quitamos la primera línea de la cabecera
+  return a.map( s=>Number(s.split(",")[1] )); // Nos quedamos sólo con los valores numéricos
+}
+
+/**
+ * Genera una gráfica de barras horizontales
+ * @param {Array} arrDatos - Array de valores numéricos 
+ */
+const generarGráfica = arrDatos => {
+
+  let max = Math.max( ...arrDatos );
+  let factorMult = 500/max;
+
+  for(let gasto of arrDatos){
+    output.innerHTML+=`<div style="width:${factorMult * gasto}px">${gasto}</div>`;
+  }
+}
+
 /////////////////
 // MAIN
 //////////////////
 
+// Variable que contiene los datos del fichero
+let contenido;
+
+// Código que leer de un fichero
 const fileInput = document.querySelector("input[type=file]");
 const output = document.querySelector(".output");
 
@@ -11,6 +35,8 @@ fileInput.addEventListener("change", async () => {
   const [file] = fileInput.files;
 
   if (file) {
-    output.innerText = await file.text();
+    contenido = await file.text();
+    let arrDatos = generarArrayDatos(contenido);
+    generarGráfica(arrDatos);
   }
 });
