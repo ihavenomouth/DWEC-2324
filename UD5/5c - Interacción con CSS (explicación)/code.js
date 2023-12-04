bf24111e587c581f52d26597eb290de20e079f99
Javier <javmanfer@gmail.com>
2023-12-04 20:27:31 +0100

    CSS

---
+++ UD5/5c - Interacción con CSS (explicación)/code.js
@@ -0,0 +1,38 @@
+"use strict";
+
+
+//////////////////
+// FUNCTIONS
+//////////////////
+
+
+///////////////////
+// MAIN
+/////////////////////
+let btn = document.querySelector("#btnAccion");
+let divResultado = document.querySelector("#divResultado");
+
+
+// btn.style="border-radius: 5px"
+// btn.style.borderRadius= "5px";
+// btn.style.paddingLeft = "30px";
+
+// divResultado.innerHTML+=`<p>${btn.classList}</p>`;
+// divResultado.innerHTML+=`<p>${btn.classList[0]}</p>`;
+
+// btn.classList="btn info";
+// btn.classList.replace("danger", "success");
+
+// btn.classList.toggle("danger");
+// btn.classList.toggle("danger");
+
+
+
+//// Hojas de estilo ///
+// document.styleSheets[0].cssRules[5].style.backgroundColor="yellow"
+// document.styleSheets[0].deleteRule(5)
+
+// document.styleSheets[0].insertRule("button { color: white }", 1); 
+
+// Cambiar una hoja de estilos por otra
+// document.querySelector("#theme-link").href="styleDark.css"; 
